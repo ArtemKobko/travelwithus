346f7bc1ea81fe203c0733e2927aed50a69c1508
Artem Kobko <arteks505@gmail.com>
2023-06-10 16:19:28 +0200

    add destitations-page

--- src/components/Locations/index.jsx
+++ src/components/Locations/index.jsx
@@ -2,50 +2,72 @@ import React from 'react';
 import { IoLocationOutline } from 'react-icons/io5';
 import { BsArrowRight } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import classes from './Locations.module.scss';
 import locationData from '../../utils/locationData';
 
-function Locations() {
-  const mapData = locationData;
+function Locations({ destinationsPage }) {
+  const mapData = destinationsPage ? locationData : locationData.slice(0, 6);
   return (
     <div className={classes.container}>
-      <div className={classes.destinations}>
-        <div className={classes.destinations__content}>
-          <h2 className={classes.destinations__content__title}>Most visited</h2>
-          <div className={classes.destinations__content__locations}>
+      <div className={classes.locations}>
+        <div className={classes.locations__content}>
+          <h2 className={classes.locations__content__title}>{ destinationsPage ? 'All destunations' : 'Most visited' }</h2>
+          <div className={destinationsPage
+            ? classes.locations__content__countries : classes.locations__content__countriesHP}
+          >
             {mapData.map(({
-              id, location, isSummer, img,
+              id, location, isSummer, img, price, description,
             }) => (
-              <div className={classes.locations__item} key={id}>
+              <div className={classes.country} key={id}>
                 <img
                   src={img}
                   alt={location}
-                  className={classes.locations__item__img}
+                  className={classes.country__img}
                 />
                 <div className={classes.overlay} />
-                <div className={classes.locations__item__content}>
-                  <IoLocationOutline />
-                  <h2 className={classes.locations__item__content__location}>
-                    {location}
-                  </h2>
+                <div className={classes.country__content}>
+                  <div className={classes.country__content__location}>
+                    <h2>
+                      <IoLocationOutline />
+                      {location}
+                    </h2>
+                    {destinationsPage ? <p>{price}</p> : null}
+                  </div>
+                  {destinationsPage ? (
+                    <>
+                      <div className={classes.country__content__line} />
+                      <p className={classes.country__content__description}>
+                        {description}
+                      </p>
+                      <div className={classes.country__content__line} />
+                      <button type="button" className={classes.country__content__button}>Book now</button>
+                    </>
+                  ) : null}
+
                 </div>
                 {isSummer && (
-                <div className={classes.locations__item__tag}>Summer 2023</div>
+                <p className={classes.country__tag}>Summer 2023</p>
                 )}
               </div>
             ))}
           </div>
-          <div className={classes.destinations__content__viewmore}>
+          {(!destinationsPage && (
+          <div className={classes.locations__content__viewmore}>
             <Link to="/destinations">
               view more
-              {' '}
               <BsArrowRight />
             </Link>
           </div>
+          ))}
         </div>
       </div>
     </div>
   );
 }
+
+Locations.propTypes = {
+  destinationsPage: PropTypes.bool.isRequired,
+};
 
 export default Locations;
